a27a5a7ca72a29b8024b934aede67d433ee168d7
joshex150 <joshex150@gmail.com>
2022-10-30 04:03:13 +0100

    gg

--- models/Product.js
+++ models/Product.js
@@ -2,22 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-	name: String,
-	price: Number,
-	timeStamp: { seconds: Number, nanoseconds: Number },
-	category: String,
-	description: String,
-	discountPercent: Number,
-	features: [String],
-	id: String,
-	imageUrls: [String],
-	inStock: Boolean,
-	quantity: Number,
-	size: [String],
-	piece: Number,
-	collection: String,
-	discountPrice: Number,
-	favclicks: Number
+  name: { type: String, required: false },
+  price: { type: Number, required: false },
+  timeStamp: {
+    seconds: { type: Number, required: false },
+    nanoseconds: { type: Number, required: false },
+  },
+  category: { type: String, required: false },
+  description: { type: String, required: false },
+  discountPercent: { type: Number, required: false },
+  features: { type: [String], required: false },
+  id: { type: { type: String, required: false }, required: false },
+  imageUrls: { type: [String], required: false },
+  inStock: { type: Boolean, required: false },
+  quantity: { type: Number, required: false },
+  size: { type: [String], required: false },
+  piece: { type: Number, required: false },
+  collection: { type: String, required: false },
+  discountPrice: { type: Number, required: false },
+  favclicks: { type: Number, required: false },
 });
 
 const Product = mongoose.model("Product", ProductSchema);
